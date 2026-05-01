8488ec03fda7b7d944ab188b0fd54c82105efb30
Juan Diego Mariscal Millan <juandiegomariscal@gmail.com>
2026-05-01 20:59:56 +0200

    build

---
+++ install.sh
@@ -0,0 +1,38 @@
+# update
+
+sudo dnf update -y
+
+# Configurar git
+read -p "Introduce tu nombre para Git: " git_name
+read -p "Introduce tu email para Git: " git_email
+
+git config --global user.name "$git_name"
+git config --global user.email "$git_email"
+
+# utilidades
+dnf install pipx unzip xclip yq jq fzf -y
+
+# peticiones http
+dnf install curl wget httpie -y
+
+# vscode
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc &&
+  echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo >/dev/null
+dnf check-update &&
+  sudo dnf install code -y
+
+# Nerd fonts
+wget -q https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip
+unzip -o JetBrainsMono.zip -d ~/.local/share/fonts
+fc-cache -f -v
+
+# gh cli
+sudo dnf install dnf5-plugins
+sudo dnf config-manager addrepo --from-repofile=https://cli.github.com/packages/rpm/gh-cli.repo
+sudo dnf install gh --repo gh-cli
+
+# entorno javascript
+
+sudo dnf install npm nodejs
+
+curl -fsSL https://bun.sh/install | bash
